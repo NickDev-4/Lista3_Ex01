3679ad0808a699257158ff0131acf074b1f0952a
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-03-18 10:37:07 -0300

    Exercicio01

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+double milhas, Km;
+
+Console.Write("Digite uma distância em milhas: ");
+milhas = double.Parse(Console.ReadLine());
+
+Km = milhas * 1.609;
+
+Console.WriteLine("");
+
+Console.WriteLine("{0} milhas são {1}Km", milhas, Km);
